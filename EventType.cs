fe55ee12a45c80dfa6db0b2c68c26288edb6a161
rockiking <rockiking7@gmail.com>
2025-11-21 10:59:50 +0300

    Добавил код классов enums
    LogLevel это класс который классифицирует  события по важности и позволяет фильтровать логи по уровню критичности
    EventType же говорит о типах событий, она группирует события по бизнес-смыслу, упрощает поиск и фильтрацию конкретных типов событий.

---
+++ EventType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationModule.Enums
+{
+    public enum EventType
+    {
+        // Системные события
+        ApplicationStart,
+        ApplicationStop,
+
+        // Бизнес-события💅
+        CreateRecord,
+        UpdateRecord,
+        DeleteRecord,
+        StatusChange,
+        DeadlineChange,
+
+        // Технические события
+        DataLoad,
+        DataSave,
+        ConfigurationChange,
+
+        // Ошибки
+        AuthenticationError,
+        ValidationError,
+        SystemError
+    }
+}
---
+++ LogLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationModule.Enums
+{
+    public enum LogLevel
+    {
+        Info,      // Информационное сообщение
+        Warning,   // Предупреждение
+        Error      // Ошибка
+    }
+}
